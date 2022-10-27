a43c43db1c08a64dc6a065bf6b76dca2bbc1c2a7
Ubuntu <ubuntu@ip-172-31-6-61.eu-west-3.compute.internal>
2022-10-27 21:14:25 +0000

    Chatfunctie Website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'nl#home'
   get 'nl/legal'
+  get 'nl/typeform'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
